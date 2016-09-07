e760133c63eebe820d9250daea7046574a81950a
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2016-09-07 18:38:41 -0400

    Update slam.pages.js

--- slam.pages.js
+++ slam.pages.js
@@ -1,5 +1,3 @@
-var slamVars = {};
-
 String.prototype.replaceAll = function(find, replace) {
   return this.replace(new RegExp(find.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1"), 'g'), replace);
 };
