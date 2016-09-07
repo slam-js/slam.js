ab0cff825f828ac6f124a47228cd0144ee246b29
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2016-09-07 18:38:12 -0400

    Update slam.pages.js

--- slam.pages.js
+++ slam.pages.js
@@ -4,3 +4,6 @@ String.prototype.replaceAll = function(find, replace) {
   return this.replace(new RegExp(find.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1"), 'g'), replace);
 };
 
+function slamIt(find,replace) {
+  document.getElementsByTagName("*").innerHTML = document.getElementsByTagName("*").innerHTML.replaceALL(find,replace);
+}
