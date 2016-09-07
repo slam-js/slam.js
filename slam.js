81763d638c6b551420d40d1eed0b2ba0f7e0aca8
Hampton <herohamp@users.noreply.github.com>
2016-09-07 18:00:26 -0400

    Update slam.js

--- slam.js
+++ slam.js
@@ -1,1 +1,26 @@
+var querys = window.location.search.slice(1).split('&').reduce(function _reduce (/*Object*/ a, /*String*/ b) {b = b.split('=');a[b[0]] = decodeURIComponent(b[1]);return a;}, {});
+console.log(querys);
 
+var page;
+
+if (querys[Object.keys(querys)[0]]!= "undefined") {
+  page="/";
+} else {
+  page=Object.keys(querys)[0];
+}
+
+var routes = {}
+
+
+function routeAdd(route, pagelocation) {
+  routes[route]={"route":route, "pagelocation":pagelocation}
+}
+
+function runRoutes() {
+  for (i in routes) {
+    if (routes[i].route==page) {
+    
+      console.log(routes[i].route);
+    }
+  }
+}
